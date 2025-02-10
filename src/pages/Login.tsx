b0cf34e7698b0d4a1bfffb5f7380e1ac57dc3156
JoashRautraut <rautrautjoash1@gmail.com>
2025-02-11 02:22:54 +0800

    finally recognized by ionic

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,39 +1,36 @@
+
 import { 
-    IonButtons,
-    IonButton,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar,
-      useIonRouter
-  } from '@ionic/react';
+  IonButton,
+  IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar, 
+    useIonRouter
+} from '@ionic/react';
 
-  const Login: React.FC = () => {
-    const navigation = useIonRouter();
-     
+const Login: React.FC = () => {
+  const navigation = useIonRouter();
+
   const doLogin = () => {
-    navigation.push('/it35-lab/app','forward','replace');
-
+      navigation.push('/it35-lab/app','forward','replace');
   }
-  
-    return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        <IonButton onClick={() => doLogin()} expand="full">
-          Login
-        </IonContent>
-      </IonPage>
-    );
-  };
-  
-  export default Login;
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className='ion-padding'>
+          <IonButton onClick={() => doLogin()} expand="full">
+              Login
+          </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
