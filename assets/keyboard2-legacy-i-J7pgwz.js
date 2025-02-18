6bb3ce97edd5e21bb6a605f444e7d41e25ab49a8
JoashRautraut <rautrautjoash1>
2025-02-18 08:46:40 +0800

    Updates

--- assets/keyboard2-legacy-i-J7pgwz.js
+++ assets/keyboard2-legacy-i-J7pgwz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DgHPubzp.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-CGZaFr2W.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-BkTie6vC.js
+++ assets/swipe-back-legacy-BkTie6vC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DgHPubzp.js","./index3-legacy--ttWCL7f.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-CGZaFr2W.js","./index3-legacy-aaJcXY8H.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
