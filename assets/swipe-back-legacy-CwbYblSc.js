49285561dc3a53af6e0a47e1db28073501370abf
JoashRautraut <rautrautjoash1@gmail.com>
2025-02-25 14:09:51 +0800

    Updates

--- assets/swipe-back-legacy-CwbYblSc.js
+++ assets/swipe-back-legacy-CwbYblSc.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BrSjxtzR.js","./index3-legacy-CCNOPBu2.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DVxIh_g9.js","./index3-legacy-BFOfaQR3.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
