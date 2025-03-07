47ef59d633a6b3e0ac9bae795a74a65ee2534f14
JoashRautraut <rautrautjoash1@gmail.com>
2025-03-07 21:30:20 +0800

    Updates

--- assets/ios.transition-legacy-Dvplk47l.js
+++ assets/ios.transition-legacy-Dvplk47l.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DVxIh_g9.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-t85y3C4V.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
