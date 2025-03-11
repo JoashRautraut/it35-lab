ea12bca3bc03b9be4e9e9d1d09185488a392218d
JoashRautraut <rautrautjoash1@gmail.com>
2025-03-11 08:55:14 +0800

    Updates

--- assets/keyboard2-legacy-DCMJkGvA.js
+++ assets/keyboard2-legacy-DCMJkGvA.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-t85y3C4V.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-BhOgrHrZ.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
