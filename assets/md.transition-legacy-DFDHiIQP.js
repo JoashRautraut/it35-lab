06cac1277bd01b8f02cc637a8926bc961f97bb34
JoashRautraut <rautrautjoash1@gmail.com>
2025-04-08 08:55:20 +0800

    Updates

--- assets/md.transition-legacy-DFDHiIQP.js
+++ assets/md.transition-legacy-DFDHiIQP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CfQu4uFu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DrS1QOEH.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
