6fb36f37abf1b4e0547913fa6ac6e515a8cc1191
JoashRautraut <rautrautjoash1@gmail.com>
2025-04-28 10:59:46 +0800

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 
-
-
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -35,7 +33,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 import './theme/variables.css';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
-import About from './pages/About';
+import Register from './pages/Register';
 
 setupIonicReact();
 
@@ -43,9 +41,9 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-      <Route exact path="/it35-lab" component={Login} />
-      <Route exact path="/it35-lab/app" component={Menu} />
-      
+        <Route exact path="/it35-lab" component={Login} />
+        <Route exact path="/it35-lab/register" component={Register} />
+        <Route path="/it35-lab/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
