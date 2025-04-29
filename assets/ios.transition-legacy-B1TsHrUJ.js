02158596fe5060a9f039026cc9ae2092bac3932a
JoashRautraut <rautrautjoash1@gmail.com>
2025-04-29 09:00:15 +0800

    Updates

--- assets/ios.transition-legacy-B1TsHrUJ.js
+++ assets/ios.transition-legacy-B1TsHrUJ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C7gZv8ss.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BPnk_cpe.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
