92d7b5b315c97df256de96b499ac92486875fa2e
JoashRautraut <rautrautjaosh1@gmail.com>
2025-05-15 12:55:19 +0800

    Updates

--- assets/md.transition-legacy-CCEr-59Q.js
+++ assets/md.transition-legacy-CCEr-59Q.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BPnk_cpe.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DDOL1h5g.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
